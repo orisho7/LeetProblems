428bb074b434f932e85cf01c8705cb1da9704338
Abdullrazaq naqsho <apoodi.805@gmail.com>
2026-03-10 03:56:42 +0300

    0001-TWO-SUM Stats: Time: 27 ms (11.28%), Space: 5.4 MB (92.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,28 @@
+func twoSum(nums []int, target int) []int {
+	for index, value := range nums {
+		wanted := target
+		if index == len(nums)-1 {
+			break
+		}
+
+		sum := value + nums[index+1]
+
+		if sum == wanted {
+			return []int{index, index + 1}
+		} else {
+
+			for j := index + 1; j < len(nums); j++ {
+				if index == len(nums)-1 {
+					break
+				}
+				sum := value + nums[j]
+				if sum == wanted {
+					return []int{index, j}
+				}
+
+			}
+		}
+	}
+	return []int{}
+
+}
