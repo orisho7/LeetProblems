e2dd72a74aa896f800335f1d7110aefd591b1da4
Abdullrazaq naqsho <apoodi.805@gmail.com>
2026-03-10 17:58:44 +0300

    2820-RETURN-LENGTH-OF-ARGUMENTS-PASSED Stats: Time: 35 ms (90.71%), Space: 53.9 MB (30.58%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+ args = [5]
+var argumentsLength = function(...args) {
+    return args.length
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
