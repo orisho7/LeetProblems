7d35c9a80ad7c95e820e36feec4908432966b269
Abdullrazaq naqsho <apoodi.805@gmail.com>
2026-03-10 04:19:18 +0300

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 5.9 MB (63.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -12,3 +12,34 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
+//old logic
+func twoSumloop(nums []int, target int) []int {
+	for index, value := range nums {
+		wanted := target
+		if index == len(nums)-1 {
+			break
+		}
+
+		sum := value + nums[index+1]
+		fmt.Println("outside", wanted, index, value, sum)
+
+		if sum == wanted {
+			return []int{index, index + 1}
+		} else {
+
+			for j := index + 1; j < len(nums); j++ {
+				if index == len(nums)-1 {
+					break
+				}
+				sum := value + nums[j]
+				fmt.Println("inside loop", wanted, index, value, sum)
+				if sum == wanted {
+					return []int{index, j}
+				}
+
+			}
+		}
+	}
+	return []int{}
+
+}
